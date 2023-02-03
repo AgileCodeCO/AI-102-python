72bce99ed3c3b262e1b6f1c757ab6514fb35b456
JORGE A. GONZALEZ <jorge.gonzalez@agilecode.co>
2023-02-03 16:46:22 -0500

    Custom vision for classification complete

--- custom-computer-vision/image-classification.py
+++ custom-computer-vision/image-classification.py
@@ -4,7 +4,7 @@ from azure.cognitiveservices.vision.customvision.prediction import CustomVisionP
 from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
 from msrest.authentication import ApiKeyCredentials
 import time
-
+import os
 import sys
 sys.path.insert(0, '../')
 from resource_credentials import (endpoint, 
@@ -15,37 +15,55 @@ from resource_credentials import (endpoint,
     custom_vision_prediction_key,
     custom_vision_prediction_resource_id)
 
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
 # Training credentials
 credentials = ApiKeyCredentials(in_headers={"Training-key": subscription_key})
 trainer = CustomVisionTrainingClient(endpoint, credentials)
 
-# TODO: Upload images
-projects = trainer.get_projects()
-project = None
+print("Creating project...")
 
-for item in projects:
-    if item.name == "Sports":
-        project = item
+project = trainer.create_project("Sports Full")
 
-print(project.id)
+tags = ["baseball", "basketball", "bmx", "boxing", "football", "formula1", "table-tennis", "tennis", "ultimate", "volleyball"]
+
+image_list = []
+
+print("Adding images...")
+
+for tag in tags:
+    # create tag in project
+    project_tag = trainer.create_tag(project.id, tag)
+
+    # upload images
+    for filename in os.listdir(f"images/train/{tag}"):
+        with open(f"images/train/{tag}/{filename}", "rb") as image_contents:
+            image_list.append(ImageFileCreateEntry(name=filename, contents=image_contents.read(), tag_ids=[project_tag.id]))
+
+
+for batch in chunker(image_list, 64):
+    print("Uploading batch...")
+    upload_result = trainer.create_images_from_files(project.id, ImageFileCreateBatch(images=batch))
+    if not upload_result.is_batch_successful:
+        print("Image batch upload failed.")
+        for image in upload_result.images:
+            print("Image status: ", image.status)
+        exit(-1)
 
 print("Training...")
-iteration = None
-try:
-    iteration = trainer.train_project(project.id, force_train=False)
-    while iteration.status != "Completed":
-        iteration = trainer.get_iteration(project.id, iteration.id)
-        print("Training status: " + iteration.status)
-        time.sleep(1)
-    print("Training done!")
-except:
-  print("Model already trained")
-  iteration = trainer.get_iterations(project.id)[0]
+
+iteration = trainer.train_project(project.id, force_train=False)
+while iteration.status != "Completed":
+    iteration = trainer.get_iteration(project.id, iteration.id)
+    print("Training status: " + iteration.status)
+    time.sleep(1)
+print("Training done!")
 
 publish_iteration_name = "Iteration from Python"
-if iteration.publish_name == "":    
-    trainer.publish_iteration(project.id, iteration.id, publish_iteration_name, custom_vision_prediction_resource_id)
-    print("Publish done!")
+
+trainer.publish_iteration(project.id, iteration.id, publish_iteration_name, custom_vision_prediction_resource_id)
+print("Publish done!")
 
 # Prediction credentials
 prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": custom_vision_prediction_key})
